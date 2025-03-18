d3807cc2b093b32430b5cd4f4c9ef2db97ef7f29
Mykolai Skydan <117273969+ewqwdd@users.noreply.github.com>
2025-03-18 14:11:33 +0200

    Update index.js

--- index.js
+++ index.js
@@ -205,6 +205,7 @@ app.get("/subscription-success/:id", async (req, res) => {
         const email = subscriptionData.subscriber.email_address;
 
         const member = await memberstack.members.addFreePlan({
+            id,
             data: {
                 id,
                 planId: 'pln_test-paypal-r84s0jhl',
